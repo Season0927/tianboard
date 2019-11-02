966cdaadab3bde445f68efc5d99648d4dd8d6bac
Xu Jiangbo <xjbwd@163.com>
2019-11-02 11:06:43 +0800

    Format the source code.

--- Drivers/User/Inc/imu.h
+++ Drivers/User/Inc/imu.h
@@ -17,60 +17,60 @@
 
 typedef struct
 {
-	int16_t ax;
-	int16_t ay;
-	int16_t az;
+  int16_t ax;
+  int16_t ay;
+  int16_t az;
 
-	int16_t mx;
-	int16_t my;
-	int16_t mz;
+  int16_t mx;
+  int16_t my;
+  int16_t mz;
 
-	int16_t temp;
+  int16_t temp;
 
-	int16_t gx;
-	int16_t gy;
-	int16_t gz;
-	
-	int32_t ax_offset;
-	int32_t ay_offset;
-	int32_t az_offset;
+  int16_t gx;
+  int16_t gy;
+  int16_t gz;
 
-	int32_t gx_offset;
-	int32_t gy_offset;
-	int32_t gz_offset;
+  int32_t ax_offset;
+  int32_t ay_offset;
+  int32_t az_offset;
+
+  int32_t gx_offset;
+  int32_t gy_offset;
+  int32_t gz_offset;
 } mpu_data_t;
 
 typedef struct
 {
-	int16_t ax;
-	int16_t ay;
-	int16_t az;
+  int16_t ax;
+  int16_t ay;
+  int16_t az;
 
-	int16_t mx;
-	int16_t my;
-	int16_t mz;
+  int16_t mx;
+  int16_t my;
+  int16_t mz;
 
-	float temp;
+  float temp;
 
-	float wx; /*!< omiga, +- 2000dps => +-32768  so gx/16.384/57.3 =	rad/s */
-	float wy;
-	float wz;
+  float wx; /*!< omiga, +- 2000dps => +-32768  so gx/16.384/57.3 =	rad/s */
+  float wy;
+  float wz;
 
-	float vx;
-	float vy;
-	float vz;
-  
+  float vx;
+  float vy;
+  float vz;
+
   float q[4];
-  
-	float rol;
-	float pit;
-	float yaw;
+
+  float rol;
+  float pit;
+  float yaw;
 } imu_t;
 
 extern mpu_data_t mpu_data;
-extern imu_t      imu;
+extern imu_t imu;
 
-uint8_t   mpu_device_init(void);
+uint8_t mpu_device_init(void);
 void init_quaternion(void);
 void mpu_get_data(void);
 void imu_ahrs_update(void);
@@ -78,5 +78,3 @@ void imu_attitude_update(void);
 void mpu_offset_call(void);
 
 #endif
-
-
--- Src/pid.c
+++ Src/pid.c
@@ -1,53 +1,54 @@
 #include "pid.h"
 
-#define ABS(x)		((x>0)? (x): (-x)) 
+#define ABS(x) ((x > 0) ? (x) : (-x))
 
-void abs_limit(float *a, float ABS_MAX){
-    if(*a > ABS_MAX)
-        *a = ABS_MAX;
-    if(*a < -ABS_MAX)
-        *a = -ABS_MAX;
+void abs_limit(float *a, float ABS_MAX)
+{
+  if (*a > ABS_MAX)
+    *a = ABS_MAX;
+  if (*a < -ABS_MAX)
+    *a = -ABS_MAX;
 }
 
 void PidInit(
-    Pid_t *pid, 
+    Pid_t *pid,
     uint32_t mode,
     uint32_t maxout,
     uint32_t intergral_limit,
-    float 	kp, 
-    float 	ki, 
-    float 	kd)
-{  
-    pid->IntegralLimit = intergral_limit;
-    pid->MaxOutput = maxout;
-    pid->pid_mode = mode;
-    
-    pid->p = kp;
-    pid->i = ki;
-    pid->d = kd;
+    float kp,
+    float ki,
+    float kd)
+{
+  pid->IntegralLimit = intergral_limit;
+  pid->MaxOutput = maxout;
+  pid->pid_mode = mode;
+
+  pid->p = kp;
+  pid->i = ki;
+  pid->d = kd;
 }
 
-float PidCalc(Pid_t* pid, float get, float set)
+float PidCalc(Pid_t *pid, float get, float set)
 {
   pid->get[NOW] = get;
   pid->set[NOW] = set;
   pid->err[NOW] = set - get;
 
-  if(pid->pid_mode == POSITION_PID)
+  if (pid->pid_mode == POSITION_PID)
   {
     pid->pout = pid->p * pid->err[NOW];
     pid->iout += pid->i * pid->err[NOW];
-    pid->dout = pid->d * (pid->err[NOW] - pid->err[LAST] );
+    pid->dout = pid->d * (pid->err[NOW] - pid->err[LAST]);
     abs_limit(&(pid->iout), pid->IntegralLimit);
     pid->pos_out = pid->pout + pid->iout + pid->dout;
     abs_limit(&(pid->pos_out), pid->MaxOutput);
     pid->last_pos_out = pid->pos_out;
   }
-  else if(pid->pid_mode == DELTA_PID)
+  else if (pid->pid_mode == DELTA_PID)
   {
     pid->pout = pid->p * (pid->err[NOW] - pid->err[LAST]);
     pid->iout = pid->i * pid->err[NOW];
-    pid->dout = pid->d * (pid->err[NOW] - 2*pid->err[LAST] + pid->err[LLAST]);
+    pid->dout = pid->d * (pid->err[NOW] - 2 * pid->err[LAST] + pid->err[LLAST]);
 
     abs_limit(&(pid->iout), pid->IntegralLimit);
     pid->delta_u = pid->pout + pid->iout + pid->dout;
@@ -62,5 +63,5 @@ float PidCalc(Pid_t* pid, float get, float set)
   pid->get[LAST] = pid->get[NOW];
   pid->set[LLAST] = pid->set[LAST];
   pid->set[LAST] = pid->set[NOW];
-  return pid->pid_mode==POSITION_PID ? pid->pos_out : pid->delta_out;
+  return pid->pid_mode == POSITION_PID ? pid->pos_out : pid->delta_out;
 }
